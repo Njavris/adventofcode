8f2f8d88726319237881cd4caa85416ee1d1ff35
Nauris Brunins <naurisb@mikrotik.com>
2023-01-17 11:53:06 +0200

    d22: make pt1 code take less space

--- 2022/day22/day22.cpp
+++ 2022/day22/day22.cpp
@@ -20,15 +20,14 @@ class MoMap {
 		eUp = 3,
 	};
 	struct coord {
-		int x;
-		int y;
+		int x, y;
 		struct coord &operator+=(const struct coord &s) { x += s.x; y += s.y; return *this; };
 		friend struct coord operator+(struct coord l, const struct coord &r) { l += r; return l; };
 	};
 	inline int cToIdx(const struct coord &c) { return c.x + c.y * xMax; };
 	void walk(struct coord &pos, int dir, std::string steps) {
-		int dx = 0, dy = 0, s;
 		struct coord d = { 0, 0 };
+		int s;
 		sscanf(steps.c_str(), "%d", &s);
 		if (dir == eRight) d.x = 1;
 		else if (dir == eDown) d.y = 1;
@@ -37,48 +36,27 @@ class MoMap {
 		for (int i = 0; i < s; i++) {
 			struct coord nc = pos + d;
 			if (d.x) {
-				if (nc.x >= xMax) {
-					nc.x = 0;
-				} else if (nc.x < 0) {
-					nc.x = xMax - 1;
-				} else if (!map[cToIdx(nc)]) {
-					if (d.x > 0)
-						nc.x = 0;
-					if (d.x < 0)
-						nc.x = xMax - 1;
-				} else {
-					goto wrapped;
-				}
+				if (nc.x >= xMax) nc.x = 0;
+				else if (nc.x < 0) nc.x = xMax - 1;
+				else if (!map[cToIdx(nc)]) nc.x = (d.x > 0) ? 0 : xMax - 1;
+				else goto wrapped;
 			} else if (d.y) {
-				if (nc.y >= yMax) {
-					nc.y = 0;
-				} else if (nc.y < 0) {
-					nc.y = yMax - 1;
-				} else if (!map[cToIdx(nc)]) {
-					if (d.y > 0)
-						nc.y = 0;
-					if (d.y < 0)
-						nc.y = yMax - 1;
-				} else {
-					goto wrapped;
-				}
+				if (nc.y >= yMax) nc.y = 0;
+				else if (nc.y < 0) nc.y = yMax - 1;
+				else if (!map[cToIdx(nc)]) nc.y = (d.y > 0) ? 0 : yMax - 1;
+				else goto wrapped;
 			}
-			for (int j = 0; (j < (d.x ? xMax : yMax)) && !map[cToIdx(nc)]; j++)
-				nc += d;
+			for (int j = 0; (j < (d.x ? xMax : yMax)) && !map[cToIdx(nc)]; j++) nc += d;
 wrapped:
-			if (map[cToIdx(nc)] == '#')
-				break;
+			if (map[cToIdx(nc)] == '#') break;
 			pos = nc;
 		}
 	};
 	std::string getInstrToken(std::string instr, int *off) {
-		int i = *off;
-		int start = i;
+		int i = *off, start = i;
 		do {
-			if (instr[i] >= '0' && instr[i] <= '9')
-				continue;
-			if (i == *off)
-				i++;
+			if (instr[i] >= '0' && instr[i] <= '9') continue;
+			if (i == *off) i++;
 			break;
 		} while (instr[i++] != std::string::npos);
 		*off = i;
@@ -92,16 +70,12 @@ wrapped:
 		int off = 0;
 		while (off < instr.size()) {
 			std::string tkn = getInstrToken(instr, &off);
-			if (tkn[0] == 'R')
-				dir = (dir + 1) & 3;
-			else if (tkn[0] == 'L')
-				dir = (dir - 1) & 3;
-			else
-				walk(pos, dir, tkn);
+			if (tkn[0] == 'R') dir = (dir + 1) & 3;
+			else if (tkn[0] == 'L') dir = (dir - 1) & 3;
+			else walk(pos, dir, tkn);
 		}
 		partOneVal = (pos.y + 1) * 1000 + 4 * (pos.x + 1) + dir;
-		if (partOneVal != 181128)
-			std::cout << "Wrong!" << std::endl;
+		if (partOneVal != 181128) std::cout << "Wrong!" << std::endl;
 		timer.stopTiming();
 		std::cout << "Time taken by part One: " << timer.getTimeStr() << std::endl;
 	};
