a48788c6d4f742f042df0fdb6cbde5ae3b17060d
Nauris Brunins <naurisb@mikrotik.com>
2023-01-18 18:58:25 +0200

    investigationg rotations

--- 2022/day22/day22.cpp
+++ 2022/day22/day22.cpp
@@ -7,11 +7,127 @@
 
 #include "utils.h"
 
+class Vec {
+public:
+	int x, y, z;
+	Vec() : x(0), y(0), z(0) {};
+	Vec(int x1, int x2, int x3) : x(x1), y(x2), z(x3) {};
+	std::string toStr(void) {
+		return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
+	};
+	Vec &operator+=(const Vec &s) { x += s.x; y += s.y; z += s.z; return *this; };
+	friend Vec operator+(Vec l, const Vec &r) { l += r; return l; };
+	Vec &operator*=(const int c) { x *= c; y *= c; z *= c; return *this; };
+	friend Vec operator*(Vec l, const int c) { l *= c; return l; };
+	Vec &operator*=(const Vec &s) { x *= s.x; y *= s.y; z *= s.z; return *this; };
+	friend Vec operator*(Vec l, const Vec &r) { l.x *= r.x; l.y *= r.y; l.z *= r.z; return l; };
+	Vec cross(const Vec &s) {
+		return Vec(y * s.z - z * s.y, -1 * (x * s.z - z * s.x), x * s.y - y * s.x);
+	};
+	Vec normal(void) {
+		return Vec(x ? (x / std::abs(x)) : 0, y ? (y / std::abs(y)) : 0, z ? (z / std::abs(z)) : 0);
+	};
+};
+
+class Mat {
+public:
+	union {
+		/*
+			0 1 2
+			3 4 5
+			6 7 8
+		*/
+		int el[9];
+		Vec col[3];
+	};
+	Mat() { for (int i = 0; i < sizeof(el) / sizeof(el[0]); i++) el[i] = 0; };
+	Mat& multiply(const Mat &m) {
+		int res[9];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				int val = 0;
+				for (int k = 0; k < 3; k++)
+					val += el[i + (k * 3)] * m.el[k + (i * 3)];
+				res[i + (j * 3)] = val;
+			}
+		}
+		for (int i = 0; i < sizeof(el) / sizeof(el[0]); i++) el[i] = res[i];
+		return *this;
+	};
+	Mat& operator*=(const Mat &m) { return multiply(m); };
+	Vec multiply(const Vec &v) {
+		return Vec(	el[0] * v.x + el[1] * v.y + el[2] * v.z,
+				el[3] * v.x + el[4] * v.y + el[5] * v.z,
+				el[6] * v.x + el[7] * v.y + el[8] * v.z);
+	};
+	static inline int sinSq(int angle) {
+		if (angle == 0 || angle == 180 || angle == 360 || angle == -180 || angle == -360)
+			return 0;
+		if (angle == 90 || angle == -270)
+			return 1;
+		if (angle == -90 || angle == 270)
+			return -1;
+		std::cout << "Something Wrong! " << angle << std::endl;
+		exit(0);
+	};
+	static inline int cosSq(int angle) {
+		if (angle == 0 || angle == 360 || angle == -360)
+			return 1;
+		if (angle == 180 || angle == -180)
+			return -1;
+		if (angle == 90 || angle == -270 || angle == -90 || angle == 270)
+			return 0;
+		std::cout << "Something Wrong! " << angle << std::endl;
+		exit(0);
+	};
+	static Mat rotMat(int a, int b, int g) {
+		Mat rot = Mat();
+		rot.el[0] = cosSq(b) * cosSq(g);
+		rot.el[1] = sinSq(a) * sinSq(b) * cosSq(g) - cosSq(a) * sinSq(g);
+		rot.el[2] = cosSq(a) * sinSq(b) * cosSq(g) + sinSq(a) * sinSq(g);
+
+		rot.el[3] = cosSq(b) * sinSq(g);
+		rot.el[4] = sinSq(a) * sinSq(b) * sinSq(g) + cosSq(a) * cosSq(g);
+		rot.el[5] = cosSq(a) * sinSq(b) * sinSq(g) - sinSq(a) * cosSq(g);
+
+		rot.el[6] = -sinSq(b);
+		rot.el[7] = sinSq(a) * cosSq(b);
+		rot.el[8] = cosSq(a) * cosSq(b);
+		return rot;
+	};
+	static Mat rotMat(int angle, Vec axis) {
+		Mat rot = Mat();
+		int cs = cosSq(angle);
+		int sn = sinSq(angle);
+		rot.el[0] = axis.x * (1 - cs) + cs;
+		rot.el[1] = axis.y * axis.x * (1 - cs) + axis.z * sn;
+		rot.el[2] = axis.z * axis.z * (1 - cs) - axis.y * sn;
+
+		rot.el[3] = axis.x * axis.y * (1 - cs) - axis.z * sn;
+		rot.el[4] = axis.y * (1 - cs) + cs;
+		rot.el[5] = axis.z * axis.z * (1 - cs) + axis.x * sn;
+
+		rot.el[6] = axis.x * axis.z * (1 - cs) + axis.y * sn;
+		rot.el[7] = axis.y * axis.z * (1 - cs) - axis.x * sn;
+		rot.el[8] = axis.z * (1 - cs) + cs;
+		return rot;
+	}
+	std::string toStr(void) {
+		std::string ret;
+		for (int i = 0; i < sizeof(el) / sizeof(el[0]); i++) {
+			if (i && !(i%3))
+				ret += "\n";
+			ret += std::to_string(el[i]) + " ";
+		}
+		return ret;
+	};
+};
+
 class MoMap {
 	int64_t partOneVal;
 	int64_t partTwoVal;
 	std::string instr;
-	int yMax = 0, xMax = 0;
+	int yMax = 0, xMax = 0, sz;
 	char *map;
 	enum {
 		eRight = 0,
@@ -19,14 +135,10 @@ class MoMap {
 		eLeft = 2,
 		eUp = 3,
 	};
-	struct coord {
-		int x, y;
-		struct coord &operator+=(const struct coord &s) { x += s.x; y += s.y; return *this; };
-		friend struct coord operator+(struct coord l, const struct coord &r) { l += r; return l; };
-	};
-	inline int cToIdx(const struct coord &c) { return c.x + c.y * xMax; };
-	void walk(struct coord &pos, int dir, std::string steps) {
-		struct coord d = { 0, 0 };
+	inline int cToIdx(int x, int y, int z) { return x + y * xMax + z * xMax * yMax; };
+	inline int cToIdx(const Vec &v) { return cToIdx(v.x, v.y, v.z); };
+	void walk(Vec &pos, int dir, std::string steps) {
+		Vec d = Vec();
 		int s;
 		sscanf(steps.c_str(), "%d", &s);
 		if (dir == eRight) d.x = 1;
@@ -34,7 +146,7 @@ class MoMap {
 		else if (dir == eLeft) d.x = -1;
 		else if (dir == eUp) d.y = -1;
 		for (int i = 0; i < s; i++) {
-			struct coord nc = pos + d;
+			Vec nc = pos + d;
 			if (d.x) {
 				if (nc.x >= xMax) nc.x = 0;
 				else if (nc.x < 0) nc.x = xMax - 1;
@@ -64,10 +176,9 @@ wrapped:
 	};
 	void doPartOne(void) {
 		Timer timer = Timer();
-		struct coord pos = { 0 };
-		int dir = eRight;
+		Vec pos = Vec();
+		int dir = eRight, off = 0;
 		walk(pos, dir, "1");
-		int off = 0;
 		while (off < instr.size()) {
 			std::string tkn = getInstrToken(instr, &off);
 			if (tkn[0] == 'R') dir = (dir + 1) & 3;
@@ -79,8 +190,82 @@ wrapped:
 		timer.stopTiming();
 		std::cout << "Time taken by part One: " << timer.getTimeStr() << std::endl;
 	};
+
 	void doPartTwo(void) {
 		Timer timer = Timer();
+		Vec pos = Vec();
+		int dir = eRight, off = 0;
+		walk(pos, dir, "1");
+		std::cout << xMax << "," << yMax << " " << sz << std::endl;
+		for (int y = 0; y < yMax; y++) {
+			for (int x = 0; x < xMax; x++) {
+				Mat r = Mat::rotMat(0, 0, 0);
+				Vec v = Vec(x, y, 0);
+				Vec rot = r.multiply(v);
+//				std::cout << v.toStr() << "->" << rot.toStr() << " ";
+				char c = map[cToIdx(x, y, 0)];
+				if (c) 	std::cout << c << "(" << x << "," << y << ") ";
+				else std::cout << "       ";
+			}
+			std::cout << std::endl;
+		}
+
+		std::cout << std::endl;
+		for (int y = 0; y < yMax; y++) {
+			for (int x = 0; x < xMax; x++) {
+				int i = x / sz;
+				int j = y / sz;
+				char c = map[cToIdx(x,y, 0)];
+				if (c) 	std::cout << c << "(" << i << "," << j << ") ";
+				else std::cout << "       ";
+			}
+			std::cout << std::endl;
+		}
+
+		std::cout << std::endl;
+		/* wrap around X axis */
+		for (int y = 0; y < yMax; y++) {
+			for (int x = 0; x < xMax; x++) {
+				int angle = 90 * (y / sz);
+				Vec v = Vec((x % sz), (y % sz), 0);
+				Mat rm = Mat::rotMat(angle, Vec(1, 0, 0));
+				Vec r = rm.multiply(v);
+				r *= Vec(1, 1, -1);
+				if (angle == 90 || angle == 180)
+					r += Vec(0, sz - 1, 0);
+				if (angle == 180 || angle == 270)
+					r += Vec(0, 0, sz - 1);
+				char c = map[cToIdx(x, y, 0)];
+				if (c) 	std::cout << c << r.toStr() << " ";
+				else std::cout << "         ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+		/* wrap around Y axis */
+		for (int y = 0; y < yMax; y++) {
+			for (int x = 0; x < xMax; x++) {
+				int angle = 90 * (x / sz);
+				Vec v = Vec((x % sz), (y % sz), 0);
+				Mat rm = Mat::rotMat(angle, Vec(0, 1, 0));
+				Vec r = rm.multiply(v);
+//				r *= Vec(1, 1, -1);
+				if (angle == 90 || angle == 180)
+					r += Vec(sz - 1, 0, 0);
+				if (angle == 180 || angle == 270)
+					r += Vec(0, 0, sz - 1);
+				char c = map[cToIdx(x, y, 0)];
+				if (c) 	std::cout << c << r.toStr() << " ";
+				else std::cout << "         ";
+			}
+			std::cout << std::endl;
+		}
+		while (off < instr.size()) {
+			std::string tkn = getInstrToken(instr, &off);
+		}
+		partTwoVal = (pos.y + 1) * 1000 + 4 * (pos.x + 1) + dir;
+		if (partTwoVal != 52311)
+			std::cout << "Wrong!" << std::endl;
 		timer.stopTiming();
 		std::cout << "Time taken by part Two: " << timer.getTimeStr() << std::endl;
 	};
@@ -90,6 +275,7 @@ public:
 		std::vector<std::string> input;
 		while (in.nextLine()) {
 			std::string ln = in.getLine();
+			std::cout << ln << std::endl;
 			if (!ln.size()) {
 				in.nextLine();
 				instr = in.getLine();
@@ -102,10 +288,11 @@ public:
 		}
 		map = new char[xMax * yMax];
 		memset(map, 0, xMax * yMax);
+		sz = ((xMax > yMax) ? xMax : yMax) / 4;
 		for (int y = 0; y < input.size(); y++)
 			for (int x = 0; x < input[y].size(); x++)
 				if (input[y][x] != ' ')
-					map[cToIdx({x,y})] = input[y][x];
+					map[cToIdx(x, y, 0)] = input[y][x];
 
 		doPartOne();
 		doPartTwo();
