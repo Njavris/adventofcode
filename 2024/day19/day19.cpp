1c4bab8d850d3524860014e7a5ed1d71e73fc60f
user <user@email>
2024-12-28 20:34:08 +0200

    improve d19

--- 2024/day19/day19.cpp
+++ 2024/day19/day19.cpp
@@ -2,88 +2,27 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include <algorithm>
-#include <unordered_map>
 
 using namespace std;
 
-bool searchP1(int exclude, vector<string> &towels, string pattern, int offset = 0) {
-	int ret = 0;
-	if (offset == pattern.size())
-		return true;
-	string str = pattern.substr(offset);
-	for (int i = 0; i < towels.size(); i++) {
-		if (i == exclude)
-			continue;
-		auto &t = towels[i];
-		if (t[0] != str[0] || t.size() > str.size())
-			continue;
-		if (str.starts_with(t)) {
-			if (searchP1(exclude, towels, str, t.size()))
-				return true;
-		}
-
-	}
-	return false;
-}
-
-int partOne(vector<string> towels, vector<string> patterns) {
-	int ret = 0;
-	
-	sort(begin(towels), end(towels));
-	for (int i = 0; i < towels.size(); i++) {
-		if (searchP1(i, towels, towels[i])) {
-			towels.erase(begin(towels) + i);
-			i --;
-		}
-	}
-
-	for (auto &p : patterns)
-		if (searchP1(-1, towels, p))
-			ret++;
-	return ret;
-}
-
-long int searchP2(unordered_map<int, long int> &cache,
-			vector<string> &towels, string pat, int off = 0);
-long int caching(unordered_map<int, long int> &cache,
-			vector<string> &towels, string pat, int off = 0) {
+long int search(long int *cache, vector<string> &towels, string pat, int off = 0) {
+	long int ret = 0;
 	string tgt = pat.substr(off);
-	int key = tgt.size();
-	try {
-		return cache.at(key);
-	}
-	catch (out_of_range) {
-		cache[key] = searchP2(cache, towels, tgt);
-	}
-	return cache.at(key);
-}
-
-long int searchP2(unordered_map<int, long int> &cache,
-			vector<string> &towels, string pat, int off) {
-	long int ret = 0;
 
 	for (int i = 0; i < towels.size(); i++) {
 		auto &t = towels[i];
-		if (!pat.starts_with(t))
+		if (!tgt.starts_with(t))
 			continue;
-		if (pat.size() == t.size())
+
+		if (tgt.size() == t.size()) {
 			ret++;
-		else
-			ret += caching(cache, towels, pat, t.size());
+		} else {
+			int k = tgt.size() - t.size() - 1;
+			if (cache[k] == ~0)
+				cache[k] =  search(cache, towels, tgt, t.size());
+			ret += cache[k];
+		}
 	}
-	return ret;
-}
-
-long int partTwo(vector<string> towels, vector<string> patterns) {
-	long int ret = 0;
-	unordered_map<int, long int> cache;
-
-	for (auto &p : patterns) {
-		ret += caching(cache, towels, p);
-		cache.clear();
-	}
-	
 	return ret;
 }
 
@@ -91,9 +30,9 @@ int main(int argc, char **argv) {
 	ifstream ifs(argc == 2 ? argv[1] : "input");
 
 	vector<string> towels;
-	vector<string> patterns;
 
 	string ln;
+
 	getline(ifs, ln);
 	for (int i = 2, s = 0; i <= ln.size(); i++) {
 		if (ln.substr(i - 2, 2) == ", " || i == ln.size()) {
@@ -103,11 +42,23 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	while (ifs >> ln)
-		patterns.push_back(ln);
+	int partOne = 0;
+	long int partTwo = 0;
 
-	cout << "Part One: " << partOne(towels, patterns) << endl;
-	cout << "Part Two: " << partTwo(towels, patterns) << endl;
+	while (ifs >> ln) {
+		long int *cache = new long int[ln.size()];
+		memset(cache, ~0, ln.size() * sizeof(long int));
+
+		unsigned long ret = search(cache, towels, ln);
+		if (ret)
+			partOne++;
+		partTwo += ret;
+
+		delete[] cache;
+	}
+
+	cout << "Part One: " << partOne << endl;
+	cout << "Part Two: " << partTwo << endl;
 
 	return 0;
 }
