a1ebb86ef4d9e79626b2e2a51f34d60668b0179f
user <user@email>
2025-01-01 21:11:46 +0200

    2024 d20 p2

--- 2024/day20/day20.cpp
+++ 2024/day20/day20.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -12,13 +11,11 @@ public:
 	v2(int x, int y) : x(x), y(y) {};
 	v2 &operator+=(v2 const &r) { x += r.x; y += r.y; return *this; } 
 	v2 operator+(v2 const &r) { v2 ret = *this; return ret += r; } 
-	v2 &operator-=(v2 const &r) { x -= r.x; y -= r.y; return *this; } 
-	v2 operator-(v2 const &r) { v2 ret = *this; return ret -= r; } 
-	bool operator==(v2 const &r) const { return x == r.x && y == r.y; } 
 };
 
+v2 dirs[] = { v2(1, 0), v2(0, 1), v2(-1, 0), v2(0, -1) };
+
 void route(vector<vector<int>> &map, v2 curr) {
-	v2 dirs[] = { v2(1, 0), v2(0, 1), v2(-1, 0), v2(0, -1) };
 	auto &v = map[curr.y][curr.x];
 
 	for (auto &d : dirs) {
@@ -31,39 +28,62 @@ void route(vector<vector<int>> &map, v2 curr) {
 	}
 }
 
-void cheatP1(vector<vector<int>> &map, vector<int> &cheats) {
-	for (int y = 1; y < map.size() - 1; y++) {
-		for (int x = 1; x < map[y].size(); x++) {
-			auto &c = map[y][x];
-			auto &l = map[y][x - 1];
-			auto &r = map[y][x + 1];
-			auto &d = map[y + 1][x];
-			auto &u = map[y - 1][x];
+void backtrack(vector<vector<int>> &map, v2 p, vector<v2> &path) {
+	auto &v = map[p.y][p.x];
+	path.push_back(p);
 
-			if (c >= 0)
-				continue;
-
-			if (l > 0 && r > 0) {
-				int v = abs(l - r) - 2;
-				if (v >= 100)
-					cheats.push_back(v);
-			}
-			if (u > 0 && d > 0) {
-				int v = abs(u - d) - 2;
-				if (v >= 100)
-					cheats.push_back(v);
-			}
-		}
+	for (auto &d : dirs) {
+		v2 np = p + d;
+		auto &nv = map[np.y][np.x];
+		if (nv == v - 1)
+			return backtrack(map, np, path);
 	}
 }
 
-void cheatP2(vector<vector<int>> &map, vector<int> &cheats) {
+int cheatP1(vector<vector<int>> &map, vector<v2> &path) {
+	int ret = 0;
+	for (int i = 0; i < path.size(); i++) {
+		v2 &p1 = path[i];
+		int &v1 = map[p1.y][p1.x];
+		for (int j = i + 1; j < path.size(); j++) {
+			v2 &p0 = path[j];
+			int &v0 = map[p0.y][p0.x];
+			int dX = p1.x - p0.x;
+			int dY = p1.y - p0.y;
+			int wX = 0, wY = 0;
+			if (dY == 0 && abs(dX) == 2)
+				wX = dX / 2 > 0 ? p1.x + 1 : p1.x - 1; 
+			if (dX == 0 && abs(dY) == 2)
+				wY = dY / 2 > 0 ? p1.y + 1 : p1.y - 1; 
+			if (((wX || wY) && (map[wY][wX] == -1)) && ((v1 - v0 - 1) >= 100))
+				ret++;
+		}
+	}
+	return ret;
+}
+
+int cheatP2(vector<vector<int>> &map, vector<v2> &path) {
+	int ret = 0;
+	for (int i = 0; i < path.size(); i++) {
+		v2 &p1 = path[i];
+		int &v1 = map[p1.y][p1.x];
+		for (int j = i + 1; j < path.size(); j++) {
+			v2 &p0 = path[j];
+			int &v0 = map[p0.y][p0.x];
+			int dXY = abs(p0.x - p1.x) + abs(p0.y - p1.y);
+			if (dXY > 20)
+				continue;
+			if (v1 - v0 - dXY >= 100)
+				ret ++;
+		}
+	}
+	return ret;
 }
 
 int main(int argc, char **argv) {
 	ifstream ifs(argc == 2 ? argv[1] : "input");
 	vector<vector<int>> map;
-	vector<int> cheats;
+	vector<v2> path;
 
 	v2 start, finish;
 	string ln;
@@ -78,19 +98,13 @@ int main(int argc, char **argv) {
 				finish = v2(x, y);
 		}
 	}
-
 	map[start.y][start.x] = 1;
+
 	route(map, start);
+	backtrack(map, finish, path);
 
-	cheatP1(map, cheats);
-	cout << map[finish.y][finish.x] << endl;
-	cout << "Part One: " << cheats.size() << endl;
-	cheats.clear();
-
-	cheatP2(map, cheats);
-	cout << "Part Two: " << cheats.size() << endl;
-
-
+	cout << "Part One: " << cheatP1(map, path) << endl;
+	cout << "Part Two: " << cheatP2(map, path) << endl;
 
 	return 0;
 }
