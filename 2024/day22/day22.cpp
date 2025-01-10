9a4d720c0abd85b1e070db725d783d65400c398e
user <user@debian.home>
2025-01-10 15:02:10 +0200

    2024 22 p2

--- 2024/day22/day22.cpp
+++ 2024/day22/day22.cpp
@@ -2,12 +2,13 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 long int partOne(vector<int> &input) {
 	long int ret = 0;
-	for_each(begin(input), end(input), [&ret] (uint32_t secret) {
+	for_each(begin(input), end(input), [&ret] (unsigned secret) {
 		for (int i = 0; i < 2000; i++) {
 			secret ^= (secret << 6) & 0xffffff;
 			secret ^= (secret >> 5) & 0xffffff;
@@ -19,7 +20,43 @@ long int partOne(vector<int> &input) {
 };
 
 long int partTwo(vector<int> &input) {
-	return 0;
+	vector<unordered_map<unsigned, int>> maps;
+	for_each(begin(input), end(input), [&maps] (unsigned secret) {
+		maps.push_back(unordered_map<unsigned, int>());
+		auto &buyer = maps.back();
+		unsigned seq = 0;
+		int prev = 0;
+		for (int i = 0; i < 2000; i++) {
+			secret ^= (secret << 6) & 0xffffff;
+			secret ^= (secret >> 5) & 0xffffff;
+			secret ^= (secret << 11) & 0xffffff;
+			int curr = secret % 10;
+			int delta = curr - prev;
+			prev = curr;
+			seq <<= 8;
+			seq |= (delta & 0xff);
+			if (i > 2 && !buyer.count(seq))
+				buyer[seq] = curr;
+		}
+	});
+
+	long int ret = 0;
+	for (int i = 0; i < maps.size(); i++) {
+		auto &map = maps[i];
+		for (auto &[k,v] : map) {
+			long int s = v;
+			for (int j = i + 1; j < maps.size(); j++) {
+				auto &m = maps[j];
+				if (m.count(k)) {
+					s += m[k];
+					m.erase(k);
+				}
+			}
+			if (s > ret)
+				ret = s;
+		}
+	}
+	return ret;
 }
 
 int main(int argc, char **argv) {
